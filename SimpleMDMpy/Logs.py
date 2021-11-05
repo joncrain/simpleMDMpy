93c4ec630eb82a5494837304b8cad720008db7b9
bryan heinz <git@bryanheinz.com>
2021-11-05 03:13:02 +0000

    added params= to be explicit, and marked a potential bug.

--- SimpleMDMpy/Logs.py
+++ SimpleMDMpy/Logs.py
@@ -10,7 +10,8 @@ class Logs(SimpleMDMpy.SimpleMDM.Connection):
     def __init__(self, api_key):
         SimpleMDMpy.SimpleMDM.Connection.__init__(self, api_key)
         self.url = self._url("/logs")
-
+    
+    # FIXME: when setting limit, there may be an indefinite loop
     def get_logs(self, starting_after=None, limit=None):
         """And I mean all the LOGS"""
         url = self.url
@@ -19,4 +20,4 @@ class Logs(SimpleMDMpy.SimpleMDM.Connection):
             params['starting_after'] = starting_after
         if limit:
             params['limit'] = limit
-        return self._get_data(url, params)
+        return self._get_data(url, params=params)
